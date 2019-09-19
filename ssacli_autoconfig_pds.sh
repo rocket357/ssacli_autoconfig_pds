15abbfb8315c81b8adc190aeafe8709d59a03791
Jonathon Sisson <sisson.j@gmail.com>
2019-09-19 01:14:45 -0500

    Added clarification comments + TODO

--- ssacli_autoconfig_pds.sh
+++ ssacli_autoconfig_pds.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
+# ssacli_autoconfig_pds:
+# Finds unused pds on HPE controllers and configures single-disk raid 0's on them.
+# Primarily useful for configurations where data redundancy is handled upstream (ceph, cassandra, etc...)
+
 SSACLI=$(which ssacli)
 
 # this function finds all of the free pds on a given ctrl slot and creates a single disk raid 0 on that pd.
-# Primarily useful for configurations where data redundancy is handled upstream (ceph, cassandra, etc...)
 function build_all_free {
-        echo "Checking lds on slot $slot";
+        echo "Checking lds on slot $1";
+        
+        ## TODO - this doesn't seem to find single-disk raid 0's, ironically.
         used_pds=($($SSACLI ctrl slot=$1 ld all show detail 2>/dev/null | awk '/physicaldrive/ {print $2}'))
+        
         for pd in $($SSACLI ctrl slot=$1 pd all show | awk '/physicaldrive/ {print $2}'); do
+                
                 in_use=0;
                 count=0;
+                
+                # check that the current pd isn't in the "used_pds" array
                 while [ "x${used_pds[count]}" != "x" ]
                 do
                         if [ $pd == ${used_pds[count]} ]
@@ -17,8 +26,11 @@ function build_all_free {
                                 echo "    Skipping in-use physical disk: $pd";
                                 in_use=1;
                         fi;
+                        
                         count=$(( $count + 1 ))
                 done
+
+                # if this pd is indeed unused, build a raid 0 on it
                 if [ $in_use -eq 0 ]
                 then
                         echo "    Building raid 0 on unused pd $pd";
