c09aac9dd9cd6650fadc3445412954c6a3cce4e4
Jonathon Sisson <sisson.j@gmail.com>
2019-09-19 00:50:11 -0500

    Centralize ssacli path to a single variable

--- ssacli_autoconfig_pds.sh
+++ ssacli_autoconfig_pds.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+SSACLI=$(which ssacli)
+
 # this function finds all of the free pds on a given ctrl slot and creates a single disk raid 0 on that pd.
 # Primarily useful for configurations where data redundancy are handled upstream (ceph, cassandra, etc...)
 function build_all_free {
         echo "Checking lds on slot $slot";
-        used_pds=($(ssacli ctrl slot=$1 ld all show detail 2>/dev/null | awk '/physicaldrive/ {print $2}'))
-        for pd in $(ssacli ctrl slot=$1 pd all show | awk '/physicaldrive/ {print $2}'); do
+        used_pds=($($SSACLI ctrl slot=$1 ld all show detail 2>/dev/null | awk '/physicaldrive/ {print $2}'))
+        for pd in $($SSACLI ctrl slot=$1 pd all show | awk '/physicaldrive/ {print $2}'); do
                 in_use=0;
                 count=0;
                 while [ "x${used_pds[count]}" != "x" ]
@@ -20,12 +22,12 @@ function build_all_free {
                 if [ $in_use -eq 0 ]
                 then
                         echo "    Building raid 0 on unused pd $pd";
-                        yes y | ssacli ctrl slot=$1 create type=ld drives=$pd raid=0 >/dev/null 2>&1;
+                        yes y | $SSACLI ctrl slot=$1 create type=ld drives=$pd raid=0 >/dev/null 2>&1;
                 fi;
         done
 }
 
 # find slots and pass their numbers to build_all_free above.
-for slot in $(ssacli ctrl all show | awk '/Slot/ {print substr($0,index($0,"Slot")+5,1)}'); do
+for slot in $($SSACLI ctrl all show | awk '/Slot/ {print substr($0,index($0,"Slot")+5,1)}'); do
         build_all_free $slot;
 done
